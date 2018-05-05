1d2d7bb725bf3835b1d4458a1da80c1b5c12b80b
Trinh Duc Tai <trinhductai6869@gmail.com>
2018-05-05 11:10:57 +0700

    chinh sua jquery cho nut

--- 1.js
+++ 1.js
@@ -1,4 +1,22 @@
 ﻿ $(function(){
- 
+ 	vitricuaedu = $('#khoiedu').offset().top;
+ 		console.log(vitricuaedu);
+ 	$(".xuong").click(function() {
+ 		$('body,html').animate({scrollTop:vitricuaedu});
+ 		  // return false;
+ 	});
+ 	$(".kn").click(function() {
+ 		$('body,html').animate({scrollTop:$('#khoikn').offset().top})
+ 		// return false;
+ 	});
+ 	$(".sp").click(function() {
+ 		$('body,html').animate({scrollTop:$('#khoisp').offset().top})
+ 		// return false;
+ 	});
+ 	$(".lh").click(function() {
+ 		$('body,html').animate({scrollTop:$('#khoilh').offset().top})
+ 		// return false;
+ 	});
+
 })  
  
