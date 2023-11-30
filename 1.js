17a0f86f85cbe83baaf0fd089ff7e202e663f28b
Trinh-Duc-Tai <trinhductai6869@gmail.com>
2023-11-30 10:13:40 +0700

    # css for cursor

--- 1.js
+++ 1.js
@@ -214,3 +214,21 @@ $(window).scroll(function(){
 })
 
 // het progress Bar 
+
+
+// ---------- js for cursor -----------
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
+
+// ---------- het js for cursor ---------- 
