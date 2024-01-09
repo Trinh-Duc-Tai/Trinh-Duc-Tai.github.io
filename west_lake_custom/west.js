0061799180ece923e6683fb9f3fd8441406e3f97
Trinh-Duc-Tai <trinhductai6869@gmail.com>
2024-01-09 09:48:30 +0700

    demo web west lake

--- west_lake_custom/west.js
+++ west_lake_custom/west.js
@@ -1,13 +1,112 @@
-// Thêm vào cuối tệp script.js hoặc trong thẻ <script> trong index.html
-function openModal(imgElement) {
-    var modal = document.getElementById('modal');
-    var modalImg = document.getElementById('modal-img');
+// ----------------
+$(document).ready(function () {
+    $('[data-fancybox="overview"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+  
+    $('[data-fancybox="lobby"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+  
+    $('[data-fancybox="restaurant"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+    $('[data-fancybox="garden"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+    $('[data-fancybox="pool"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+    $('[data-fancybox="gym"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+    $('[data-fancybox="children_playground"]').fancybox({
+      thumbs: {
+        autoStart: true,
+      },
+      buttons: [
+        'thumbs',
+        'close'
+      ],
+      infobar: false,
+      keyboard: true,
+      idleTime: false,
+      clickContent: function(current, event) {
+        return current.type === 'image' ? 'zoom' : false;
+      },
+    });
+  });
+// ---------------
 
-    modal.style.display = 'block';
-    modalImg.src = imgElement.src;
-}
-
-function closeModal() {
-    var modal = document.getElementById('modal');
-    modal.style.display = 'none';
-}
